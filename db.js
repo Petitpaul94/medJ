07a444e6094a0efdc87e9fca0aa604aa1b00aa7c
PetitPaul94 <icoeuille@gmail.com>
2020-05-26 13:35:12 +0200

    db finalisée

--- db.js
+++ db.js
@@ -1,12 +1,36 @@
-const mysql = require('mysql');
+function dbCall(titre, commence_le) {
 
-var con = mysql.createConnection({
-	host : 'localhost',
-	user : 'student',
-	password : 'socrate'
-});
+  const mysql = require('mysql');
+  //Formatage variables db
+  const firstYear = Boolean(commence_le > new Date(2020, 8, 1));
+  commence_le = commence_le.slice(0, 10);
+  const matiere = titre.slice(0, /\s/.exec(titre).index);
+  const nom = titre.slice(/\s/.exec(titre).index + 1);
 
-con.connect(function(err) {
-	if (err) throw err;
-	console.log('COnnected !');
-});
+  var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'student',
+    password: 'socrate',
+    database: 'medecine'
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    con.query(`SELECT * FROM matieres WHERE nom="${matiere}"`, function(err, result, fields) {
+
+      if (result.length != 0) {
+        return result[0].couleur;
+      } else {
+        con.query('SELECT COUNT(nom) AS count FROM matieres', function(err, result) {
+          let couleur = result[0].count + 1;
+          con.query(`INSERT INTO matieres VALUES ("${matiere}", ${couleur})`, function(err, result) {
+            if (err) throw err;
+            return couleur;
+          });
+        });
+      }
+    });
+  });
+
+}
+
